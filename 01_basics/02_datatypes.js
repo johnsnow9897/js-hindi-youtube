8b3b52802160003d5c32bfef82a946694be1e795
johnsnow9897 <166494591+johnsnow9897@users.noreply.github.com>
2024-04-18 16:43:42 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,23 @@
 
 // alert(3 + 3) // we are using nodejs, not browser
 // code readabiity should be high
+
+/*
+let name="Hitesh"
+let age=18
+letisLogged = false
+let state = null 
+*/
+
+//number=>2 to power 53
+// for bug numbers "bigint"
+// sring=> ""
+// boolean => true/false
+// null in javascript is a stand alone value
+// underline =>
+// symbol=> for unique
+
+// object
+
+console.log(typeof undefined); // output will be undefined 
+console.log(typeof null); // type would be object
