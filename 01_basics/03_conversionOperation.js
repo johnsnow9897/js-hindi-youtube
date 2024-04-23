7f7db9f6a6dc4b81021195670dd54999603cd1f6
johnsnow9897 <166494591+johnsnow9897@users.noreply.github.com>
2024-04-23 11:13:07 +0000

    datatypes and conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,61 @@
-let score = "33"
+// let score = "null"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/* what will be the outpus to differnt values
+
+// 33 could easily be converted
+// "33abc" can not be converted easily
+// true=1; false=0;
+
+*/
+
+// let isLoggedIn= "vasu"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// //console.log(booleanIsLoggedIn);
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//******************* operations ***********************
+
+let value = 3
+let negvalue = -value;
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = "vasu"
+// let str3 = str1+str2;
+
+// unique situations
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);// o/p will be 122 that wrong
+// console.log(1+2+"2");// o/p will be 32
+
+/*if string is in the beginning then all the other elements 
+will be considered as a string or if the string is the last 
+ then the conversion would be considered first
+*/
+// trickkky conversions
+
+console.log(+true);
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// primitive datatypes
+
+// 7 types are there: string, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.8
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+// refernce type (non-primitive)
+
+// Arrays, Objects, Functions
